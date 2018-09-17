8c850d292e971048c6e7b9cdb9c69902455963ff
ivlasic <ivlasic@gmail.com>
2018-09-17 13:12:27 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1CbcfwlkBIqIfxqm-60u_APmjPTgx2Jsn8ccG4nU4WZ0/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/16WZDgzlyzfP3_OQ1A9gIcd-1UlvOaIHgm2zFlaiGZVI/edit#gid=0';
